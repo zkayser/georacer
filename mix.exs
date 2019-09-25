58c6487e9141e79c3914011c6dce2f9cb6397b8d
Kayser <zkayser@gmail.com>
2019-09-25 14:43:31 -0400

    Make other teams clickable in mobile device browsers

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GeoRacer.MixProject do
   def project do
     [
       app: :geo_racer,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.9.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
